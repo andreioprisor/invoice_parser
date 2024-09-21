07be2ec8ad1891d4c1e3a340ef3681f1d42b8c84
Andrei Oprisor <82113597+andreioprisor@users.noreply.github.com>
2024-09-21 22:26:05 +0300

    Update test.py

--- invoice_parser/test.py
+++ invoice_parser/test.py
@@ -1,22 +1,7 @@
-from transformers import LayoutLMv3FeatureExtractor, LayoutLMv3TokenizerFast, LayoutLMv3Processor, LayoutLMv3ForSequenceClassification
-from tqdm import tqdm
-import torch
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image, ImageDraw, ImageFont
-import numpy as np
-from sklearn.model_selection import train_test_split
-# import imgkit
-# import easyocr
-import torchvision.transforms as T
-from pathlib import Path
-import matplotlib.pyplot as plt
 import os
 # import cv2
 from typing import List
 import json
-from torchmetrics import Accuracy
-from huggingface_hub import notebook_login
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import streamlit as st
 import io
 from invoice_parser.parser import Parser
